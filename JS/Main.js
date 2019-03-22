ad765e011f7b5b5c87361acc6dfde235d8cfe1bf
JONOTHANJOESTAR <47529003+JONOTHANJOESTAR@users.noreply.github.com>
2019-03-22 11:00:57 -0400

    o
    js fpr exercise 1

--- JS/Main.js
+++ JS/Main.js
@@ -4,3 +4,20 @@ function welcomeUser(){
 	alert("Welcome! "+ name);
 
 }
+
+function WhatIsBigger(){
+  var number1 = parseInt(prompt("Pick a number"));
+  var number2 = parseInt(prompt("Pick another number"));
+   if(number1>number2){ 
+     alert(number1 + "is larger");
+     console.log(number1 + "is larger");     
+   }
+   else if(number2>number1){
+     alert(number2 + "is larger");
+     console.log(number2 + "is larger");     
+   } 
+   else{
+   	alert("ERROR")
+   }
+}
+
